a49c33ac17c11044fee9f093530ba70f2e962090
Victor Feelin <valiklfiatoff@yandex.ru>
2023-01-21 13:57:39 +0300

    Reloaded to class
    But function now is not working

--- old.py
+++ old.py
@@ -1,101 +1,81 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
-window = Tk()
-window.title('Численное дифференцирование')
-window.geometry('400x200')
-tkinter.Text(window, font="Calibri 20", bg="Black")
 
-a = DoubleVar()
-aLabel = Label(
-    text='Введите нижний предел'
-)
-aLabel.pack()
+class MainWindow(tk.Tk):
 
-aEnter = Entry(
-    textvariable=a
-)
-aEnter.pack()
+    def __init__(self):
+        super().__init__()
+        self.a = DoubleVar()
+        self.b = DoubleVar()
+        self.chosenMethod = StringVar()
+        self.methods = ('Метод прямоугольника', 'Метод трапеции', 'Метод Симпсона')
+        self.answer = DoubleVar()
 
-b = DoubleVar()
-bLabel = Label(
-    text='Введите верхний предел'
-)
-bLabel.pack()
+        self.title('Численное дифференцирование')
+        self.geometry('400x200')
 
-bEnter = Entry(
-    textvariable=b, )
-bEnter.pack()
+        self.aLabel = Label(text='Введите нижний предел')
+        self.aEnter = Entry(textvariable=self.a)
+        self.bLabel = Label(text='Введите верхний предел')
+        self.bEnter = Entry(textvariable=self.b)
+        self.chooseLabel = Label(text='Выберите метод')
 
-chooseLabel = Label(
-    text='Выберите метод'
-)
-chooseLabel.pack()
+        self.enterButton = Button(text='Рассчитать', command=self.integrate(), width=17)
 
-chosenMethod = StringVar()
-methods = ('Метод прямоугольника', 'Метод трапеции', 'Метод Симпсона')
-methodsChoose = ttk.Combobox(
-    window, textvariable=chosenMethod, width=17
-)
-methodsChoose['values'] = methods
-methodsChoose.pack()
+        self.answerLabel = Entry(textvariable=self.answer, state=tk.DISABLED)
 
-answer = DoubleVar()
+        self.methodsChoose = ttk.Combobox(self, textvariable=self.chosenMethod, width=17)
+        self.methodsChoose['values'] = self.methods
 
+        self.aLabel.pack()
+        self.aEnter.pack()
+        self.bLabel.pack()
+        self.bEnter.pack()
+        self.chooseLabel.pack()
+        self.methodsChoose.pack()
+        self.enterButton.pack()
+        self.answerLabel.pack()
 
-def diff():
-    a1 = float(aEnter.get())
-    b1 = float(bEnter.get())
-    dx = (b1-a1)/10000
-    if methodsChoose.get() == 'Метод прямоугольника':
-        y = 0
-        x = a1
-        while x < b1:
-            y = y + x*2
-            x = x+dx
-        y = y*dx
-        answer.set(y)
-        #answer.set((((a1 + b1) / 2) ** 2) * (b1 - a1))
-    elif methodsChoose.get() == 'Метод трапеции':
-        y = 0
-        x = a1 + dx
-        while x < b1:
-            y = y+(x**2)
-            x = x + dx
-        y = (y+(((a1**2)+(b1**2))/2))*dx
-        answer.set(y)
-        #answer.set((((a1 ** 2) + (b1 ** 2)) / 2) * (b1 - a1))
-    elif methodsChoose.get() == 'Метод Симпсона':
-        y1 = 0
-        x = a1 + dx
-        while x < (b1-dx):
-            y1 = y1 + (x**2)
-            x = x + 2*dx
-        y2 = 0
-        x = a1 + (2 * dx)
-        while x < (b1 - (2*dx)):
-            y2 = y2 + (x**2)
-            x = x + (2*dx)
-        y = ((b1-a1)/(30000)) * ((a1**2) + (b1**2) + (4 * y1) + (2*y2))
-        answer.set(y)
+    def integrate(self):
+        a1 = float(self.aEnter.get())
+        b1 = float(self.bEnter.get())
+        dx = (b1 - a1) / 10000
 
-        #local = ((a1 ** 2) + (4 * (((a1 + b1) / 2) ** 2)) + (b1 ** 2))
-        #answer.set(((b1 - a1) * local) * (1 / 6))
+        if self.chosenMethod.get() == 'Метод прямоугольника':
+            y = 0
+            x = a1
+            while x < b1:
+                y = y + x * 2
+                x = x + dx
+            y = y * dx
+            self.answer.set(y)
 
+        elif self.chosenMethod.get() == 'Метод трапеции':
+            y = 0
+            x = a1 + dx
+            while x < b1:
+                y = y + (x ** 2)
+                x = x + dx
+            y = (y + (((a1 ** 2) + (b1 ** 2)) / 2)) * dx
+            self.answer.set(y)
 
-enterButton = Button(
-    text='Рассчитать',
-    command=diff,
-    width=17
-)
-enterButton.pack()
+        elif self.chosenMethod.get() == 'Метод Симпсона':
+            y1 = 0
+            x = a1 + dx
+            while x < (b1 - dx):
+                y1 = y1 + (x ** 2)
+                x = x + 2 * dx
+            y2 = 0
+            x = a1 + (2 * dx)
+            while x < (b1 - (2 * dx)):
+                y2 = y2 + (x ** 2)
+                x = x + (2 * dx)
+            y = ((b1 - a1) / 30000) * ((a1 ** 2) + (b1 ** 2) + (4 * y1) + (2 * y2))
+            self.answer.set(y)
 
-answerLabel = Entry(
-    textvariable=answer,
-    state=tkinter.DISABLED
-)
-answerLabel.pack()
 
 if __name__ == '__main__':
+    window = MainWindow()
     window.mainloop()
